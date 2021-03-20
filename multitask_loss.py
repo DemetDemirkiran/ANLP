437cff4cb3a0841c8da41b72895698b0b0213f1e
DemetDemirkiran <35539445+DemetDemirkiran@users.noreply.github.com>
2021-03-20 22:43:43 +0100

    Add files via upload

---
+++ multitask_loss.py
@@ -0,0 +1,17 @@
+from torch import nn
+from torch.nn import CrossEntropyLoss, L1Loss
+
+class MultitaskLoss(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        self.score_classifier = CrossEntropyLoss()
+        self.regression = L1Loss()
+
+    def forward(self, class_pred, class_true, use_pred, use_true):
+        xent_loss = self.score_classifier(class_pred, class_true)
+        reg_loss = self.regression(use_true.unsqueeze(1).float(), use_pred)
+        return {'total_loss': xent_loss+reg_loss,
+                'xent': xent_loss,
+                'regression': reg_loss}
